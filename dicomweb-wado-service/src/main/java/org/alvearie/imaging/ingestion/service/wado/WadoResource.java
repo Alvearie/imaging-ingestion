4dc85af5b1993a7751a2c1fb0785cddadc185337
Jeesmon Jacob <jjacob@us.ibm.com>
2021-04-22 09:33:41 -0400

    Add metadata endpoint to WADO
    Signed-off-by: Jeesmon Jacob <jjacob@us.ibm.com>

--- dicomweb-wado-service/src/main/java/org/alvearie/imaging/ingestion/service/wado/WadoResource.java
+++ dicomweb-wado-service/src/main/java/org/alvearie/imaging/ingestion/service/wado/WadoResource.java
@@ -8,24 +8,40 @@ package org.alvearie.imaging.ingestion.service.wado;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.stream.JsonGenerator;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.alvearie.imaging.ingestion.model.result.DicomEntityResult;
 import org.alvearie.imaging.ingestion.service.s3.S3Service;
+import org.alvearie.imaging.ingestion.service.s3.SimpleStoreContext;
+import org.alvearie.imaging.ingestion.service.s3.StoreContext;
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.BulkData;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.VR;
+import org.dcm4che3.imageio.codec.Transcoder;
+import org.dcm4che3.json.JSONWriter;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartRelatedOutput;
@@ -56,11 +72,22 @@ public class WadoResource {
     @RestClient
     DicomQueryClient queryClient;
 
+    @Context
+    private HttpServletRequest request;
+
     @GET
     @Path("/studies/{studyUID}")
     @Produces("multipart/related")
     public void retrieveStudy(@PathParam("studyUID") String studyUID, @Suspended AsyncResponse ar) throws IOException {
         buildDicomResponse(queryClient.getResults(studyUID), ar);
+    }
+
+    @GET
+    @Path("/studies/{studyUID}/metadata")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void retrieveStudyMetadata(@PathParam("studyUID") String studyUID,
+            @QueryParam("includefields") String includefields, @Suspended AsyncResponse ar) {
+        buildMetadataResponse(queryClient.getResults(studyUID), ar);
     }
 
     @GET
@@ -83,6 +110,14 @@ public class WadoResource {
     }
 
     @GET
+    @Path("/studies/{studyUID}/series/{seriesUID}/metadata")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void retrieveSeriesMetadata(@PathParam("studyUID") String studyUID, @PathParam("seriesUID") String seriesUID,
+            @QueryParam("includefields") String includefields, @Suspended AsyncResponse ar) {
+        buildMetadataResponse(queryClient.getResults(studyUID, seriesUID), ar);
+    }
+
+    @GET
     @Path("/studies/{studyUID}/series/{seriesUID}/rendered")
     public void retrieveRenderedSeries(@PathParam("studyUID") String studyUID, @PathParam("seriesUID") String seriesUID,
             @QueryParam("annotation") String annotation, @QueryParam("quality") int quality,
@@ -97,6 +132,15 @@ public class WadoResource {
     public void retrieveInstance(@PathParam("studyUID") String studyUID, @PathParam("seriesUID") String seriesUID,
             @PathParam("objectUID") String objectUID, @Suspended AsyncResponse ar) {
         buildDicomResponse(queryClient.getResults(studyUID, seriesUID, objectUID), ar);
+    }
+
+    @GET
+    @Path("/studies/{studyUID}/series/{seriesUID}/instances/{objectUID}/metadata")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void retrieveInstanceMetadata(@PathParam("studyUID") String studyUID,
+            @PathParam("seriesUID") String seriesUID, @PathParam("objectUID") String objectUID,
+            @Suspended AsyncResponse ar) {
+        buildMetadataResponse(queryClient.getResults(studyUID, seriesUID, objectUID), ar);
     }
 
     @GET
@@ -183,6 +227,88 @@ public class WadoResource {
         ar.resume(responseBuilder.build());
     }
 
+    private void buildMetadataResponse(List<DicomEntityResult> results, AsyncResponse ar) {
+        if (results == null || results.size() == 0) {
+            Response.ResponseBuilder responseBuilder = Response.status(Response.Status.NOT_FOUND);
+            ar.resume(responseBuilder.build());
+            return;
+        }
+
+        LOG.info(String.format("Found %d instances", results.size()));
+
+        Object output = writeMetadataJSON(results);
+        Response.ResponseBuilder responseBuilder = Response.status(Response.Status.OK).entity(output)
+                .type(MediaType.APPLICATION_JSON);
+        ar.resume(responseBuilder.build());
+    }
+
+    private Object writeMetadataJSON(List<DicomEntityResult> results) {
+        return new AsyncStreamingOutput() {
+            @Override
+            public CompletionStage<Void> asyncWrite(AsyncOutputStream output) {
+                try {
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                    JsonGenerator gen = Json.createGenerator(baos);
+                    JSONWriter writer = new JSONWriter(gen);
+                    gen.writeStartArray();
+                    for (DicomEntityResult rslt : results) {
+                        writer.write(loadMetadata(rslt.getResource().getObjectName()));
+                    }
+                    gen.writeEnd();
+                    gen.flush();
+
+                    return output.asyncWrite(baos.toByteArray());
+                } catch (Exception e) {
+                    throw new WebApplicationException(exceptionAsString(e), Response.Status.INTERNAL_SERVER_ERROR);
+                }
+            }
+        };
+    }
+
+    private Attributes loadMetadata(String objectKey) throws IOException {
+        ByteArrayOutputStream baos = s3Service.getObject(objectKey);
+        try (Transcoder transcoder = new Transcoder(new ByteArrayInputStream(baos.toByteArray()))) {
+            StoreContext ctx = new SimpleStoreContext();
+            transcoder.transcode(new TranscoderHandler(ctx));
+
+            Attributes metadata = ctx.getAttributes();
+            StringBuffer sb = request.getRequestURL();
+            sb.setLength(sb.lastIndexOf("/metadata"));
+            mkInstanceURL(sb, metadata);
+            setBulkdataURI(metadata, sb.toString());
+
+            return metadata;
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    private void setBulkdataURI(Attributes attrs, String retrieveURL) {
+        try {
+            attrs.accept(new Attributes.ItemPointerVisitor() {
+                @Override
+                public boolean visit(Attributes attrs, int tag, VR vr, Object value) {
+                    if (tag == Tag.PixelData) {
+                        attrs.setValue(tag, vr, new BulkData(null, retrieveURL, false));
+                    }
+                    return true;
+                }
+            }, true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void mkInstanceURL(StringBuffer sb, Attributes attr) {
+        if (sb.lastIndexOf("/instances/") < 0) {
+            if (sb.lastIndexOf("/series/") < 0) {
+                sb.append("/series/").append(attr.getString(Tag.SeriesInstanceUID));
+            }
+            sb.append("/instances/").append(attr.getString(Tag.SOPInstanceUID));
+        }
+    }
+
     private void addDicomPart(MultipartRelatedOutput output, String objectKey) {
         output.addPart(getDicom(objectKey), APPLICATION_DICOM_TYPE);
     }
@@ -234,5 +360,25 @@ public class WadoResource {
             }
         }
         return viewport;
+    }
+
+    private static String exceptionAsString(Exception e) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+
+    private final class TranscoderHandler implements Transcoder.Handler {
+        private final StoreContext storeContext;
+
+        private TranscoderHandler(StoreContext storeContext) {
+            this.storeContext = storeContext;
+        }
+
+        @Override
+        public OutputStream newOutputStream(Transcoder transcoder, Attributes dataset) throws IOException {
+            storeContext.setAttributes(dataset);
+            return OutputStream.nullOutputStream();
+        }
     }
 }
