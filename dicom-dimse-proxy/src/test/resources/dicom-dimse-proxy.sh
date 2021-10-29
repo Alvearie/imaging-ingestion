62315af6f9967a97de807add3516d2bf7244d739
Jeesmon Jacob <jjacob@us.ibm.com>
2021-10-29 09:21:43 -0400

    Consolidate env variables in one env file (#63)
    Signed-off-by: Jeesmon Jacob <jjacob@us.ibm.com>

--- dicom-dimse-proxy/src/test/resources/dicom-dimse-proxy.sh
+++ dicom-dimse-proxy/src/test/resources/dicom-dimse-proxy.sh
@@ -1,15 +1,8 @@
 #!/bin/bash -e
 
-export DIMSE_CONFIG_PATH=dimse-config
-export DIMSE_PROXY_PORT=<PROXY_PORT>
-export DIMSE_CALLED_AET=<TARGET_AET>
-export DIMSE_NATS_URL=<NATS_HOST>:<NATS_PORT>
-export DIMSE_NATS_TLS_ENABLED=<true|false>
-export DIMSE_CALLED_HOST=<TARGET_ARCHIVE_HOST>
-export DIMSE_CALLED_PORT=<TARGET_ARCHIVE_PORT>
-export DIMSE_NATS_SUBJECT_ROOT=<NATS_SUBJECT_ROOT>
-export DIMSE_PROXY_ACTOR=PROXY
-export DIMSE_NATS_AUTH_TOKEN=<NATS_AUTH_TOKEN>
+set -o allexport
+source ./dicom-dimse-proxy.env
+set +o allexport
 
 echo "Starting dicom-dimse-proxy in background ..."
 nohup java -jar ./dicom-dimse-proxy-*.jar 2>&1 &
